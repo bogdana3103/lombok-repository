bb180003c6ede9c3da7db56a3c3d8efd4afcf074
Bogdana Zaparnuk <zaparnukbogdana@gmail.com>
2020-04-16 14:03:34 +0300

    bug fixes

--- src/components/latestComponent/latest.jsx
+++ src/components/latestComponent/latest.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './scss/latestComponent.scss';
 import line from '../../image/line.svg';
-import jennyMarvin from '../../image/jennyMarvin.png';
-import andrewNeel from '../../image/andrewNeel.png';
-import imgBhadra from '../../image/imgBhadra.png';
+import {boxes} from '../helper';
 
 
 
@@ -15,25 +13,18 @@ function Latest() {
                 <img className='latestLine' src={line} alt={line}/>
             </div>
             <div className='latestInner'>
-                <div className='box1'>
-                   <img className='jennyMarvin' src={jennyMarvin} alt={jennyMarvin}/>
+                {boxes.map((box) =>
+                <div className='box' key={box.key}>
+                   <img className='image' src={require(`../../image/${box.name}.png`)} alt=""/>
                    <img className='boxLine' src={line} alt={line}/>
-                   <p className='textBox1'>How to use mindfullness in your everyday life</p> 
+                <p className='textBox'>{box.text}</p> 
                 </div>
-                <div className='box2'>
-                   <img className='andrewNeel' src={andrewNeel} alt={andrewNeel}/>
-                   <img className='boxLine' src={line} alt={line}/> 
-                   <p className='textBox2'>What are whe best walking meditations</p>
-                </div>
-                <div className='box3'>
-                    <img className='imgBhadra' src={imgBhadra} alt={imgBhadra}/>
-                    <img className='boxLine' src={line} alt={line}/>
-                    <p className='textBox3'>How to go deep into the mind of hope</p>
-                </div>
+                )}
             </div>
         </div>
     )
 }
+
 
 export default Latest
 
