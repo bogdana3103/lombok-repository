1879894dde879731710df0ded22a8d7e3b5d55a1
Bogdana Zaparnuk <zaparnukbogdana@gmail.com>
2020-05-06 12:59:16 +0300

    small

--- src/components/HeadComponent/head.jsx
+++ src/components/HeadComponent/head.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 import './scss/headComponent.scss';
 import logoImage from '../../image/logoImage.svg';
 import line from '../../image/line.svg';
+//import menuButton from '../../image/menuButton.svg';
+//import myFunction from '../myFunction.js';
+
 
 
 
 
 function Head () {
     return (
-        
+        <div>
         <div className='mainHead'>
-            
             <div className='menu'>
                <div className='logo'>
-                  <img className='logoImage' src={logoImage} alt={logoImage}/> 
+                  <img className='logoImage' src={logoImage} alt={logoImage}/>
                </div>
                <div className='rightMenu'>
                     <h6 className='articles'>ARTICLES</h6>
@@ -28,9 +30,24 @@ function Head () {
                    <img className='line' src={line} alt={line}/>
                </div>
         </div>
-               
-            
-       
+
+        <div class="hamburger-menu">
+  <input id="menu__toggle" type="checkbox" />
+  <label class="menu__btn" for="menu__toggle">
+    <span></span>
+  </label>
+  <ul class="menu__box">
+      <div className='mobileText'>
+    <li><a class="menu__item" href="https://web.telegram.org/#/im?p=c1139992568_1415991746564835462">About</a></li>
+    <li><a class="menu__item" href="https://web.telegram.org/#/im?p=c1139992568_1415991746564835462">Articles</a></li>
+    <li><a class="menu__item" href="https://web.telegram.org/#/im?p=c1139992568_1415991746564835462">Subscribe</a></li>
+    </div>
+  </ul>
+</div>
+
+</div>
+
+
     )
 }
 
